78c2f1cd36dca817888b9f53e458f59f908156e9
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2024-03-18 20:02:34 +0200

    feat: added device_communication_configuration interface class and device_communication_factory.

--- libkommpot/include/libkommpot.h
+++ libkommpot/include/libkommpot.h
@@ -5,13 +5,16 @@
 
 #include "export_definitions.h"
 
+#include <array>
 #include <cstdint>
+#include <memory>
 #include <string>
+#include <vector>
 
 namespace kommpot {
 
 /**
- * @brief structure containing version of kommpot library
+ * @brief structure containing version of kommpot library.
  */
 struct version
 {
@@ -50,9 +53,139 @@ private:
 
 /**
  * @brief gets current version of kommpot library.
- * @return version structure
+ * @return version structure.
  */
 auto get_version() noexcept -> version;
+
+/**
+ * @brief states types of endpoint
+ */
+enum class endpoint_type
+{
+    UNKNOWN = 0,
+    INPUT = 1,
+    OUTPUT = 2
+};
+
+/**
+ * @brief states specific configuration required for opening the device communication.
+ * @example CDC-ACM devices, FTDI devices.
+ */
+struct device_communication_configuration
+{
+    /**
+     * USB device settings.
+     */
+    uint32_t vendor_id = 0;
+    uint32_t product_id = 0;
+
+    /**
+     * FTDI connection settings.
+     */
+    uint8_t bit_mode = 0;
+    uint8_t bit_mask = 0;
+
+    /**
+     * Serial communication settings.
+     */
+    uint32_t baud_rate = 0;
+    uint8_t data_bits = 0;
+    uint8_t parity = 0;
+    uint8_t stop_bits = 0;
+};
+
+class EXPORTED device_communication
+{
+public:
+    device_communication() = default;
+    virtual ~device_communication() = default;
+
+    /**
+     * @warning states class is non-copyable.
+     */
+    device_communication(const device_communication &obj) = delete;
+    auto operator=(const device_communication &obj) -> device_communication & = delete;
+
+    /**
+     * @warning states class is non-movable.
+     */
+    device_communication(device_communication &&obj) = delete;
+    auto operator=(device_communication &&obj) -> device_communication & = delete;
+
+    /**
+     * @brief sets specific configuration that has to be applied to device_communication object.
+     * @param configuration as device_communication_configuration structure.
+     * {@link device_communication_configuration device_communication_configuration}
+     */
+    void set_configuration(const device_communication_configuration &configuration)
+    {
+        m_is_custom_configuration_set = true;
+        m_configuration = configuration;
+    }
+
+    /**
+     * @brief returns current device_communication object configuration.
+     * @return configuration as device_communication_configuration structure.
+     * {@link device_communication_configuration device_communication_configuration}
+     */
+    [[nodiscard]] virtual auto configuration() const -> device_communication_configuration
+    {
+        return m_configuration;
+    }
+
+    /**
+     * @brief opens device communication via stated id.
+     * @param id states unique id by which device may be identified.
+     * @return true if opened successfully, false if any error happened.
+     */
+    virtual auto open(const std::string &id) -> bool = 0;
+
+    /**
+     * @brief states if device is already opened.
+     * @return true if device is opened, false if not.
+     */
+    virtual auto is_open() -> bool = 0;
+
+    /**
+     * @brief closes opened device, has no effect on not opened devices.
+     */
+    virtual void close() = 0;
+
+    /**
+     * @brief reads data from specified endpoint.
+     * @param endpoint_address states address as int.
+     * @param data states buffer to which read data will be written.
+     * @param size_bytes states max buffer size.
+     * @return true if read was successful, false if any error happened.
+     */
+    virtual auto read(const int &endpoint_address, void *data, size_t size_bytes) -> bool = 0;
+
+    /**
+     * @brief writes data to specified endpoint.
+     * @param endpoint_address states address as int.
+     * @param data states buffer which will be written.
+     * @param size_bytes states max buffer size.
+     * @return true if write was successful, false if any error happened.
+     */
+    virtual auto write(const int &endpoint_address, void *data, size_t size_bytes) -> bool = 0;
+
+    [[nodiscard]] virtual auto get_error_string(const uint32_t &native_error_code) const
+        -> std::string = 0;
+
+protected:
+    /**
+     * @brief states that device doesn't have native ID and uses artificial ID for stating its
+     * uniqueness. Artificial ID is composed from the USB port path.
+     */
+    constexpr static const std::array<char, 6> M_ARTIFICIAL_ID_PREFIX = {"PORT:"};
+
+    bool m_is_custom_configuration_set = false;
+    device_communication_configuration m_configuration;
+};
+
+namespace device_communication_factory {
+auto create(int vendor_id) -> std::unique_ptr<device_communication>;
+};
 
 } // namespace kommpot
 
