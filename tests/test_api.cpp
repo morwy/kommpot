908c0324fdc0c77fb958a133e7f103f2601870d8
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2024-03-23 13:24:03 +0200

    chores: added calls for kommpot functions in tests.

--- tests/test_api.cpp
+++ tests/test_api.cpp
@@ -1,14 +1,23 @@
 // clazy:skip
 // NOLINTBEGIN
 
+#include "libkommpot.h"
+
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
 
 using namespace testing;
 
-TEST(kommpot_tests, first_test)
+TEST(kommpot_tests, library_version)
 {
-    ASSERT_THAT(true, Eq(true));
+    const kommpot::version version = kommpot::get_version();
+    ASSERT_THAT(version.to_string().size(), Gt(0));
+}
+
+TEST(kommpot_tests, empty_device_list)
+{
+    const std::vector<kommpot::device_communication> device_list = kommpot::get_device_list(0);
+    ASSERT_THAT(device_list.size(), Eq(0));
 }
 
 // NOLINTEND
