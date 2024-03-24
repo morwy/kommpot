d12d2a47deb363388d4755ccaabccd64f1416dc5
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2024-03-24 14:56:58 +0200

    chores: corrected call to kommpot::get_device_list() in tests.

--- tests/test_libusb.cpp
+++ tests/test_libusb.cpp
@@ -11,7 +11,7 @@ using namespace testing;
 TEST(kommpot_libusb, empty_device_list)
 {
     const std::vector<std::unique_ptr<kommpot::device_communication>> device_list =
-        kommpot::get_device_list(0);
+        kommpot::get_device_list();
     ASSERT_THAT(device_list.size(), Eq(0));
 }
 
