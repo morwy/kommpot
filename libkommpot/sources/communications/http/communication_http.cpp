f4a505e11f6beedd4fe58388e8f8ca34d7600499
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2026-02-28 18:03:49 +0200

    Update communication_http.cpp

--- libkommpot/sources/communications/http/communication_http.cpp
+++ libkommpot/sources/communications/http/communication_http.cpp
@@ -78,37 +78,170 @@ auto communication_http::endpoints() -> std::vector<kommpot::endpoint_informatio
 auto communication_http::read(
     const kommpot::transfer_configuration &configuration, void *data, size_t size_bytes) -> bool
 {
+    if (data == nullptr)
+    {
+        SPDLOG_LOGGER_ERROR(KOMMPOT_LOGGER,
+            "Null pointer provided for data buffer, cannot perform read operation.");
+        return false;
+    }
+
+    if (size_bytes == 0)
+    {
+        SPDLOG_LOGGER_ERROR(
+            KOMMPOT_LOGGER, "Buffer size provided is zero, cannot perform read operation.");
+        return false;
+    }
+
     if (m_connection == nullptr)
     {
+        SPDLOG_LOGGER_ERROR(
+            KOMMPOT_LOGGER, "Connection is not established, cannot perform read operation.");
         return false;
     }
 
     auto result = std::visit(
         [&](const auto &s) {
             if constexpr (std::is_same_v<std::decay_t<decltype(s)>,
-                              kommpot::bulk_transfer_configuration>)
+                              kommpot::http_transfer_configuration>)
             {
-                return true;
+                if (s.resource_path.empty())
+                {
+                    SPDLOG_LOGGER_ERROR(
+                        KOMMPOT_LOGGER, "Resource path is empty, cannot perform HTTP transfer.");
+                    return false;
+                }
+
+                switch (s.type)
+                {
+                case kommpot::http_transfer_type::GET: {
+                    const auto result = m_connection->Get(s.resource_path);
+                    if (result->status < 200 || result->status >= 300)
+                    {
+                        SPDLOG_LOGGER_ERROR(KOMMPOT_LOGGER,
+                            "HTTP GET request failed with status code {} [{}]", result->status,
+                            result->status);
+                        return false;
+                    }
+
+                    std::memcpy(
+                        data, result->body.data(), std::min(size_bytes, result->body.size()));
+
+                    return true;
+                }
+                default:
+                    return false;
+                }
             }
-            else if constexpr (std::is_same_v<std::decay_t<decltype(s)>,
-                                   kommpot::control_transfer_configuration>)
-            {
-                return true;
-            }
-            else if constexpr (std::is_same_v<std::decay_t<decltype(s)>,
-                                   kommpot::interrupt_transfer_configuration>)
-            {
-                return true;
-            }
+
+            return false;
         },
         configuration);
+
     return result;
 }
 
 auto communication_http::write(
     const kommpot::transfer_configuration &configuration, void *data, size_t size_bytes) -> bool
 {
-    return false;
+    if (data == nullptr)
+    {
+        SPDLOG_LOGGER_ERROR(KOMMPOT_LOGGER,
+            "Null pointer provided for data buffer, cannot perform write operation.");
+        return false;
+    }
+
+    if (size_bytes == 0)
+    {
+        SPDLOG_LOGGER_ERROR(
+            KOMMPOT_LOGGER, "Buffer size provided is zero, cannot perform write operation.");
+        return false;
+    }
+
+    if (m_connection == nullptr)
+    {
+        SPDLOG_LOGGER_ERROR(
+            KOMMPOT_LOGGER, "Connection is not established, cannot perform write operation.");
+        return false;
+    }
+
+    const auto data_str = std::string(static_cast<char *>(data), size_bytes);
+
+    auto result = std::visit(
+        [&](const auto &s) {
+            if constexpr (std::is_same_v<std::decay_t<decltype(s)>,
+                              kommpot::http_transfer_configuration>)
+            {
+                if (s.resource_path.empty())
+                {
+                    SPDLOG_LOGGER_ERROR(
+                        KOMMPOT_LOGGER, "Resource path is empty, cannot perform HTTP transfer.");
+                    return false;
+                }
+
+                switch (s.type)
+                {
+                case kommpot::http_transfer_type::PATCH: {
+                    auto result =
+                        m_connection->Patch(s.resource_path, data_str, std::string(), nullptr);
+                    if (result->status < 200 || result->status >= 300)
+                    {
+                        SPDLOG_LOGGER_ERROR(KOMMPOT_LOGGER,
+                            "HTTP PATCH request failed with status code {} [{}]", result->status,
+                            result->status);
+                        return false;
+                    }
+
+                    return true;
+                }
+                case kommpot::http_transfer_type::POST: {
+                    auto result =
+                        m_connection->Post(s.resource_path, data_str, std::string(), nullptr);
+                    if (result->status < 200 || result->status >= 300)
+                    {
+                        SPDLOG_LOGGER_ERROR(KOMMPOT_LOGGER,
+                            "HTTP POST request failed with status code {} [{}]", result->status,
+                            result->status);
+                        return false;
+                    }
+
+                    return true;
+                }
+                case kommpot::http_transfer_type::PUT: {
+                    auto result =
+                        m_connection->Put(s.resource_path, data_str, std::string(), nullptr);
+                    if (result->status < 200 || result->status >= 300)
+                    {
+                        SPDLOG_LOGGER_ERROR(KOMMPOT_LOGGER,
+                            "HTTP PUT request failed with status code {} [{}]", result->status,
+                            result->status);
+                        return false;
+                    }
+
+                    return true;
+                }
+                case kommpot::http_transfer_type::DELETE_E: {
+                    auto result =
+                        m_connection->Delete(s.resource_path, data_str, std::string(), nullptr);
+                    if (result->status < 200 || result->status >= 300)
+                    {
+                        SPDLOG_LOGGER_ERROR(KOMMPOT_LOGGER,
+                            "HTTP DELETE request failed with status code {} [{}]", result->status,
+                            result->status);
+                        return false;
+                    }
+
+                    return true;
+                }
+                default:
+                    return false;
+                }
+            }
+
+            return false;
+        },
+        configuration);
+
+    return result;
 }
 
 auto communication_http::get_error_string(const uint32_t &native_error_code) const -> std::string
